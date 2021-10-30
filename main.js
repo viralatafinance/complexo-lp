6577503741a6d2a012722807ab019a19d9878b06
DexREAU <andromedog2017@gmail.com>
2021-10-30 10:34:20 -0300

    v2

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+
+const loading = document.querySelector('#loading')
+
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive" || document.readyState == "complete") {
+
+        loading.style.opacity = 0;
+        loading.style.visibility = 'hidden';
+
+    }
+}
+
+
+
 let windowPosition;
 const parallaxImg = document.querySelector('.desktop_intro')
 
@@ -5,3 +19,28 @@ document.body.onscroll = function() {
     windowPosition = window.pageYOffset;
     parallaxImg.style.transform = "translateY( -" + windowPosition * 1.25 + "px)";
 }
+
+function success() {
+    const button = document.querySelector('form button');
+    const input = document.querySelector('form input');
+
+    button.innerText = "ENVIADO!"
+
+    input.value = ""
+
+}
+
+const handleSubmit = (e) => {
+    dataLayer.push({event:'Enviado'});
+    e.preventDefault()
+    let myForm = document.getElementById('form');
+    let formData = new FormData(myForm)
+    fetch('/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString()
+    }).then(() => success()).catch((error) =>
+      alert(error))
+  }
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
